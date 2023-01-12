3a233d06fef5883191b5f864590222901c80cb07
Roy Chanunkha <roychanunkha@gmail.com>
2023-01-12 12:05:36 +0200

    adding C#

---
+++ test_csharp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+var startTime = DateTime.Now;
+for(int i = 0; i < 1000000000; i++) {
+    var c = i * 2;
+}
+var endTime = DateTime.Now;
+Console.WriteLine("Execution time:" + (endTime - startTime).TotalMilliseconds + "ms");
